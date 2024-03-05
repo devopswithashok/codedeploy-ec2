38682c235af52bda593e009fe48a190dff2964b2
Ashok <devopswithashok9@gmail.com>
2024-03-05 11:35:36 +0530

    new release

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello world ! Responce From Onboard'))
-app.get('/hc', (req, res) => res.send('okay Onboard'))
+app.get('/', (req, res) => res.send('Hello world'))
+app.get('/hc', (req, res) => res.send('okay hc'))
 app.listen(3000, () => console.log('Server ready'))
